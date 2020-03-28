9dae8c5bfa122cad27d36d7bc757056ddff64f5f
Mifta Chowdhury <47373678+miftachowdhury@users.noreply.github.com>
2020-03-28 19:23:22 -0400

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -6,12 +6,12 @@ var geodata = {}
 $.getJSON('data/school_points.geojson', function (results) {
     geodata = results;
     console.log(geodata.features);
+    var schNames = geodata.features.map(function (el) {
+        return el.properties.SCHOOLNAME;
+    });
+    console.log(schNames);
 });
 
-var schNames = geodata.features.map(function (el) {
-  return el.properties.SCHOOLNAME;
-});
-console.log(schNames);
 
 var initialCenterPoint = [-73.975, 40.735]
 var initialZoom = 10
