8d1754a5cc2464df65e39267cecf9a22b14457ce
Mifta Chowdhury <mifta.chowdhury@gmail.com>
2020-03-28 22:28:41 -0400

    Add autocomplete data

---
+++ js/schools.js
@@ -0,0 +1,17 @@
+var geodata = {}
+    $.getJSON('data/school_points.geojson', function (results) {
+        geodata = results;
+        console.log(geodata.features);
+    
+    var schNames = geodata.features.map(function (el) {
+        return el.properties.SCHOOLNAME;
+    });
+    console.log(schNames);
+
+var options = {
+	data: schNames
+};
+
+$("#schoolList").easyAutocomplete(options);
+
+});
